2578e03ccfbca8528041704f751f96a0abd617e8
Haruhiko Kodo <kodo.haruhiko@mobingi.com>
2018-03-16 12:32:46 +0900

    Change color depth

--- core/rfb.js
+++ core/rfb.js
@@ -1108,7 +1108,7 @@ RFB.prototype = {
             this._fb_depth = 8;
         }
         //change force
-        this._fb_depth = 16;
+        this._fb_depth = 24;
 
         RFB.messages.pixelFormat(this._sock, this._fb_depth, true);
         this._sendEncodings();
